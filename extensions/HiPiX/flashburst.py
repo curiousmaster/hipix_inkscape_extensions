adc2e8097e7f12f1f8438215e850c28ea1655a18
Curious Master <curious.master@gmail.com>
2021-04-27 09:08:19 +0200

    changed license text in extension code

--- extensions/HiPiX/flashburst.py
+++ extensions/HiPiX/flashburst.py
@@ -2,20 +2,16 @@
 #
 # Copyright (C) 2021 Stefan Benediktsson
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
+# GNU Affero General Public License v3.0
+# Permissions of this strongest copyleft license are
+# conditioned on making available complete source code
+# of licensed works and modifications, which include
+# larger works using a licensed work, under the same
+# license. Copyright and license notices must be preserved.
+# Contributors provide an express grant of patent rights.
+# When a modified version is used to provide a service
+# over a network, the complete source code of the modified
+# version must be made available.
 """
 Generate a FlashBurst as SVG.
 """
